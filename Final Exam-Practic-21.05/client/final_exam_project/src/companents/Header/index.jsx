0b0e779b19b3e9ce438008dbe762a18b79ad0347
FeridIbrahimli <ferid.ibrahimli3101@gmail.com>
2024-05-22 12:00:17 +0400

    edit

--- Final Exam-Practic-21.05/client/final_exam_project/src/companents/Header/index.jsx
+++ Final Exam-Practic-21.05/client/final_exam_project/src/companents/Header/index.jsx
@@ -5,14 +5,14 @@ import styles from "./index.module.css"
 const Header = () => {
   return (
     <>
-     <div style={{width:"100%", margin:"0 auto"}} className="container">
+     <div style={{width:"100%", margin:" 0 auto"}} className="container">
       <header>
         <div className={styles.navbar}>
           <div className="logo">
-            <h2><Link style={{fontWeight:"400", color:"white"}} >EATWELL</Link></h2>
+            <h3><Link style={{fontWeight:"400", color:"white", letterSpacing:"2px"}} >EATWELL</Link></h3>
           </div>
           <div className="links">
-            <ul style={{display:"flex", gap:"40px"}} >
+            <ul style={{display:"flex", gap:"60px",  letterSpacing:"2px"}} >
             <li><Link style={{color:"white"}} to={"/"} >Home</Link></li>
             <li><Link to={"/about"} >About</Link></li>
             <li><Link to={"/offer"} >Offer</Link></li>
--- Final Exam-Practic-21.05/client/final_exam_project/src/pages/Home/index.jsx
+++ Final Exam-Practic-21.05/client/final_exam_project/src/pages/Home/index.jsx
@@ -4,13 +4,17 @@ import styles from "./index.module.css"
 const Home = () => {
   return (
     <>
-    <main>
-      <section>
-        <div className={styles["home-div"]}>
-          
-        </div>
-      </section>
-    </main>
+      <main>
+        <section>
+          <div className={styles["home-div"]}>
+            <div className={styles["text"]}>
+              <h1 style={{fontSize:"80px", color:"white"}}>Welcome To EatWell</h1>
+              <p style={{color:"darkGrey", padding:"20px 0", letterSpacing:"1px", fontSize:"20px"}}>Come and eat well with our delicious & healthy foods.</p>
+              <button><h3 style={{fontWeight:"500"}}>Reservation</h3></button>
+            </div>
+          </div>
+        </section>
+      </main>
     </>
   )
 }
